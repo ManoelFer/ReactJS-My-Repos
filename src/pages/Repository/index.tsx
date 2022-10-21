3b6fc307b50abbe2e6aa8a2ff792f4cfd0328d9f
ManoelFer <manoelfernandes15@gmail.com>
2022-10-21 13:25:52 -0300

    ✨ show detail

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
+import { FaArrowLeft } from 'react-icons/fa'
 
 import EndpointsGitHub from 'shared/services/gitHubEndpoints/endpoints';
-import { Container } from "./styles";
+
+import { Container, OwnerContainer, Loading, BackButton } from "./styles";
+
+interface IRepositoryInfos {
+    owner: {
+        avatar_url: string;
+        login: string;
+    };
+    name: string;
+    description: string;
+}
 
 export const Repository = () => {
     let { repository_full_name } = useParams();
 
-    const [repository, setRepository] = useState<object>({});
+    const [repository, setRepository] = useState<IRepositoryInfos>();
     const [issues, setIssues] = useState<object[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
@@ -28,9 +39,24 @@ export const Repository = () => {
         loadInfos()
     }, [repository_full_name])
 
+
+    if (loading) {
+        return (
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        )
+    }
     return (
         <Container>
-
+            <BackButton to="/">
+                <FaArrowLeft color='#000' size={35} />
+            </BackButton>
+            <OwnerContainer>
+                <img src={repository?.owner.avatar_url} alt={repository?.owner.login} />
+                <h1>{repository?.name}</h1>
+                <p>{repository?.description}</p>
+            </OwnerContainer>
         </Container>
     )
 }
